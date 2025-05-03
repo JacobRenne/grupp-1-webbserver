c0de9d605ad0b1223fff39f7560a683143f97397
Jacob Rennemark <jacobrane.se@gmail.com>
2025-05-03 23:26:35 +0200

    Create example structure for taking api info to frontend

--- frontend/src/views/singleMovie.vue
+++ frontend/src/views/singleMovie.vue
@@ -1,7 +1,31 @@
 <script setup>
+import { onMounted, ref } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute()
+const movie = ref(null)
+
+onMounted(async () => {
+    const id = route.params.id
+
+    try {
+        const res = await fetch(`http://localhost:3000/api/movies/${id}`)
+        movie.value = await res.json()
+        console.log(movie.value)
+    } catch (error) { console.error(error)}
+})
+
 
 </script>
 
 <template>
-    <h1>Does this work?</h1>
+    <div v-if="movie">
+        <h1>{{  movie.title }}</h1>
+        <p>{{ movie.releaseYear }}</p>
+    </div>
 </template>
+
+<style scoped>
+
+
+</style>

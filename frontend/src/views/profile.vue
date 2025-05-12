f12a16c496b53ad42e8b643f1fb6a936de4e173f
Kajsa Palmén <kajsa.palmen@gmail.com>
2025-05-12 14:53:37 +0200

    added styling and started user reviews in profile

--- frontend/src/views/profile.vue
+++ frontend/src/views/profile.vue
@@ -4,7 +4,9 @@ import { useRoute } from "vue-router";
 
 const route = useRoute();
 const user = ref(null);
+const reviews = ref([]);
 
+//hämta användare info
 const fetchUser = async () => {
     try {
         const res = await fetch(
@@ -16,41 +18,85 @@ const fetchUser = async () => {
     }
 };
 
-onMounted(fetchUser);
-
+// se datum som kontot skapades
 const formattedDate = computed(() =>
     user.value?.createdAt
         ? new Date(user.value.createdAt).toLocaleDateString()
         : ""
 );
+
+//hämta specifik users reviews
+const fetchUserReviews = async () => {
+    try {
+        const userId = route.params.id;
+        const res = await fetch(`http://localhost:3000/api/reviews/${userId}`);
+        reviews.value = await res.json();
+    } catch (err) {
+        console.error("Failed to fetch user reviews", err);
+    }
+};
+
+onMounted(fetchUser);
+onMounted(fetchUserReviews);
 </script>
 
 <template>
-    <!-- Koppla så deras användarnamn syns -->
-    <div v-if="user">
+    <div v-if="user" id="user-card">
         <h2>Welcome, {{ user.username }}!</h2>
-        <img
-            v-if="user.profile?.avatar"
-            :src="user.profile.avatar"
-            alt="Avatar"
-        />
-        <p>
-            <strong>Name:</strong> {{ user.profile?.firstName }}
-            {{ user.profile?.lastName }}
-        </p>
-        <p><strong>Email:</strong> {{ user.email }}</p>
-        <p><strong>Bio:</strong> {{ user.profile?.bio || "No bio yet..." }}</p>
-        <p>
-            <small>Created: {{ formattedDate }}</small>
-        </p>
+        <div id="bio">
+            <img
+                v-if="user.profile?.avatar"
+                :src="user.profile.avatar"
+                alt="Avatar"
+            />
+            <span>
+                <em>{{ user.profile?.bio || "No bio yet..." }}</em>
+            </span>
+        </div>
+        <div id="user-info">
+            <p>
+                Name: {{ user.profile?.firstName }}
+                {{ user.profile?.lastName }}
+            </p>
+            <p>Email: {{ user.email }}</p>
+            <p>
+                <small>Created: {{ formattedDate }}</small>
+            </p>
+        </div>
+        <div id="user-reviews">
+            <h3>Your reviews</h3>
+        </div>
     </div>
     <p v-else>Loading profile...</p>
 </template>
 
 <style scoped>
-p {
-    background-color: black;
+#user-card {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+#user-info {
+    width: 400px;
+    padding: 20px;
+    margin: 30px;
+    background-color: whitesmoke;
+}
+#bio {
+    flex-direction: row;
+}
+
+h3 {
     color: whitesmoke;
+    font-size: large;
+}
+p {
+    color: #000814;
+    padding: 5px;
+}
+
+span {
+    color: #8bc1db;
 }
 
 img {
